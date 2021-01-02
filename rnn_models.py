721652f58a848f367231ecc8fc8a6a262d2ade9e
De Angeli <kevindeangeli94@gmail.com>
2021-01-02 17:48:04 -0500

    ”sis”

---
+++ rnn_models.py
@@ -0,0 +1,40 @@
+'''
+Created by: Kevin De Angeli
+Email: Kevindeangeli@utk.edu
+Date: 12/30/20
+'''
+import helper
+import collections
+import numpy as np
+import project_tests as tests
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import GRU, Input, Dense, TimeDistributed, Activation, RepeatVector, Bidirectional, Dropout, LSTM
+from tensorflow.keras.layers import Embedding
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import sparse_categorical_crossentropy
+def simple_model(input_shape, output_sequence_length, english_vocab_size, french_vocab_size):
+    """
+    Build and train a basic RNN on x and y
+    :param input_shape: Tuple of input shape
+    :param output_sequence_length: Length of output sequence
+    :param english_vocab_size: Number of unique English words in the dataset
+    :param french_vocab_size: Number of unique French words in the dataset
+    :return: Keras model built, but not trained
+    """
+    # Hyperparameters
+    learning_rate = 0.005
+
+    # TODO: Build the layers
+    model = Sequential()
+    model.add(GRU(256, input_shape=input_shape[1:], return_sequences=True))
+    model.add(TimeDistributed(Dense(1024, activation='relu')))
+    model.add(Dropout(0.5))
+    model.add(TimeDistributed(Dense(french_vocab_size, activation='softmax')))
+
+    # Compile model
+    model.compile(loss=sparse_categorical_crossentropy,
+                  optimizer=Adam(learning_rate),
+                  metrics=['accuracy'])
+    return model
